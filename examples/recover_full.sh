18adbcb2a131e4041a344d30016c75af08de008b
Ziyi Xi <xiziyi@msu.edu>
2019-02-11 15:46:14 -0500

    add the script to generate full gll

---
+++ examples/recover_full.sh
@@ -0,0 +1,8 @@
+cp -r /work/05880/tg851791/stampede2/specfem/20190115/simulation_taoreg/DATABASES_MPI /scratch/05880/tg851791/tmp
+cp -r /scratch/05880/tg851791/binfile/gll/bg-tao/* /scratch/05880/tg851791/tmp/DATABASES_MPI/
+cp -r /scratch/05880/tg851791/tmp/DATABASES_MPI/* /scratch/05880/tg851791/binfile/gll/bg-tao_full/
+
+rm -rf /scratch/05880/tg851791/tmp/DATABASES_MPI/
+cp -r /work/05880/tg851791/stampede2/specfem/20190115/simulation_taoreg/DATABASES_MPI /scratch/05880/tg851791/tmp
+cp -r /scratch/05880/tg851791/binfile/gll/bg-tao-min/* /scratch/05880/tg851791/tmp/DATABASES_MPI/
+cp -r /scratch/05880/tg851791/tmp/DATABASES_MPI/* /scratch/05880/tg851791/binfile/gll/bg-tao-min_full/
