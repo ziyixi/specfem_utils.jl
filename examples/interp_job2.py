e6685aebbd1d9d4424ee9c5533073e60b9ad256f
Ziyi Xi <xiziyi@msu.edu>
2019-02-11 14:33:29 -0500

    seprate dependency

---
+++ examples/interp_job2.py
@@ -0,0 +1,17 @@
+from slurmpy import Slurm
+
+# * background+tao/crust1.0 -> background+tao/crust1.0 -> background+tao/crust1.0+min/crust2.0
+print("start job 2")
+nproc_old = 144
+old_mesh_dir = "/work/05880/tg851791/stampede2/specfem/20190115/min_h_files/DATABASES_MPI"
+old_model_dir = "/scratch/05880/tg851791/binfile/perturbation/min_perturbation"
+nproc_new = 441
+new_mesh_dir = "/work/05880/tg851791/stampede2/specfem/20190115/simulation_taoreg/DATABASES_MPI"
+new_model_dir = "/scratch/05880/tg851791/binfile/interp/s362ani_addtao-pert"
+model_tags = ",".join(["vph", "vpv", "vsh", "vsv", "eta", "qmu", "rho"])
+output_dir = "/scratch/05880/tg851791/binfile/interp/s362ani_addtao_addmin-pert"
+
+command2 = f"ibrun julia src/program/xsem_interp_mesh2.jl --nproc_old {nproc_old} --old_mesh_dir {old_mesh_dir} --old_model_dir {old_model_dir} --nproc_new {nproc_new} --new_mesh_dir {new_mesh_dir} --new_model_dir {new_model_dir} --model_tags {model_tags} --output_dir {output_dir}"
+s2 = Slurm("bg+tao", {"partition": "skx-normal",
+                      "nodes": 10, "ntasks": 441, "time": "00:60:00"})
+jobid_s2 = s2.run(command2)
