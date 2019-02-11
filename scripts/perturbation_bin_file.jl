797138774c3f364aa8ee8c12e7ce76a50ac1384a
Ziyi Xi <xiziyi@msu.edu>
2019-02-11 12:27:03 -0500

    fix typo of output dir

--- scripts/perturbation_bin_file.jl
+++ scripts/perturbation_bin_file.jl
@@ -10,13 +10,16 @@ function generate_perturbation(target_basedir::String, reference_basedir::String
     model_gll_target = zeros(Float64, NGLLX, NGLLY, NGLLZ, nspec)
     model_gll_output = zeros(Float64, NGLLX, NGLLY, NGLLZ, nspec)
     for iproc in 0:nproc - 1
+        if(iproc % 10 == 0)
+            @info "step" iproc
+        end
         for tag in tags
             sem_io_read_gll_file_1!(reference_basedir, iproc, tag, model_gll_reference)
             sem_io_read_gll_file_1!(target_basedir, iproc, tag, model_gll_target)
             
             model_gll_output = (model_gll_target .- model_gll_reference) ./ model_gll_reference
             @assert count(isnan.(model_gll_output)) == 0
-            sem_io_write_gll_file_1(reference_basedir, iproc, tag, model_gll_output)
+            sem_io_write_gll_file_1(output_basedir, iproc, tag, model_gll_output)
         end
     end
 end
@@ -30,13 +33,16 @@ function generate_real(target_basedir::String, reference_basedir::String, output
     model_gll_target = zeros(Float64, NGLLX, NGLLY, NGLLZ, nspec)
     model_gll_output = zeros(Float64, NGLLX, NGLLY, NGLLZ, nspec)
     for iproc in 0:nproc - 1
+        if(iproc % 10 == 0)
+            @info "step" iproc
+        end
         for tag in tags
             sem_io_read_gll_file_1!(reference_basedir, iproc, tag, model_gll_reference)
             sem_io_read_gll_file_1!(target_basedir, iproc, tag, model_gll_target)
             
             model_gll_output = model_gll_reference .* (1 .+ model_gll_target)
             @assert count(isnan.(model_gll_output)) == 0
-            sem_io_write_gll_file_1(reference_basedir, iproc, tag, model_gll_output)
+            sem_io_write_gll_file_1(output_basedir, iproc, tag, model_gll_output)
         end
     end
 end
